e2f94d30924896f8ca0e5fccd85d7a917d84b926
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-12-22 19:08:49 +0530

    Add initial C++ program demonstrating basic output

---
+++ Day 01/question_02.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"c++ is an Object Oriented Programming Language."<<endl;
+    cout<<"It is a high level language."<<endl;
+    cout<<"\n";
+
+    return 0;
+}
