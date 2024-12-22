84c5f5ed2bf859e653223991639a64c6ada43452
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-12-22 19:18:29 +0530

    Add C++ program for user input of name, age, height, and gender

---
+++ Day 01/question_05.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    string name;
+    int age, height;
+    char gender;
+
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<"name:"<<name<<endl;
+    cout<<"Enter your age: ";
+    cin>>age;
+    cout<<"age:"<<age<<endl;
+    cout<<"Enter your height: ";
+    cin>>height;
+    cout<<"height:"<<height<<endl;
+    cout<<"Enter your gender(M/F): ";
+    cin>>gender;
+    cout<<"Gender : "<<gender<<endl;
+    cout<<"\n";
+
+    return 0;
+
+}
