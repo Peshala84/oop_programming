59f3f7cd912d1c6ce2c8556029442ef85a2608f1
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-12-22 19:04:41 +0530

    Initialized repository for c++ programming

---
+++ Day 01/question_01.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    cout<<"Hello World!"<<endl;
+    cout<<"\n";
+
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    cout<<"Entered number is: "<<num<<endl;
+    cout<<"\n";
+
+    return 0;
+}
