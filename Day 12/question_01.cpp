82fbabf388bbd6733aabfa2d95a654d876442c9c
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-01-15 23:03:16 +0530

    Add C++ program to demonstrate template class implementation with dynamic array handling

---
+++ Day 12/question_01.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class Array
+{
+private:
+    T *ptr;
+    int size;
+
+public:
+    Array(T arr[], int s);
+    void print();
+};
+
+template <typename T>
+Array<T>::Array(T arr[], int s)
+{
+    ptr = new T[s];
+    size = s;
+    for (int i = 0; i < size; i++)
+    {
+        ptr[i] = arr[i];
+    }
+}
+
+template <typename T>
+void Array<T>::print()
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << " " << *(ptr + i);
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    string starr[4] = {"AB", "XY", "CD", "EF"};
+    Array<int> a(arr, 5);
+    a.print();
+    Array<string> b(starr, 4);
+    b.print();
+
+    return 0;
+}
